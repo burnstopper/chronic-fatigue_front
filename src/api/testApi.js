ebc4019bdb5457ca89f7ec32cb3e1452b0e475ac
Alexey <timbersaw123987@gmail.com>
2023-03-13 19:33:18 +0300

    token fixed

--- src/api/testApi.js
+++ src/api/testApi.js
@@ -1,4 +1,5 @@
 import { getCommonJsonRequestProps,  getCommonHttpRequestProps, throwHttpErrors } from "../common";
+import CookieLib from "../components/cookie-lib";
 
 export const submitTest = (answer, quiz_id) =>
   fetch(`http://localhost:3001/api/ihru/test`, {
@@ -6,17 +7,24 @@ export const submitTest = (answer, quiz_id) =>
     ...getCommonJsonRequestProps(),
     body: JSON.stringify({"answer" : answer, "quiz_id" : quiz_id}),
   }).then(response => throwHttpErrors(response))
-    .then(response => {
-      console.log(response);
-      return response.json();
-    });
+  .then(response => {
+    if (!CookieLib.getCookieToken() || CookieLib.getCookieToken().length == 0) {
+      CookieLib.setCookieToken(response.headers.get('Authorization'));
+    } 
+    return response.json(); 
+  });
 
 export const getResult = (test_id) => {
   return fetch(`http://localhost:3001/api/ihru?test_id=${test_id}`, {
     method: "GET",
     ...getCommonHttpRequestProps(),
   }).then(response => throwHttpErrors(response))
-    .then(response => response.json());
+  .then(response => {
+    if (!CookieLib.getCookieToken() || CookieLib.getCookieToken().length == 0) {
+      CookieLib.setCookieToken(response.headers.get('Authorization'));
+    } 
+    return response.json(); 
+  });
 }
 
 export const getResults = () => {
@@ -24,5 +32,10 @@ export const getResults = () => {
     method: "GET",
     ...getCommonHttpRequestProps(),
   }).then(response => throwHttpErrors(response))
-    .then(response => response.json());
+    .then(response => {
+      if (!CookieLib.getCookieToken() || CookieLib.getCookieToken().length == 0) {
+        CookieLib.setCookieToken(response.headers.get('Authorization'));
+      } 
+      return response.json(); 
+    });
 }
