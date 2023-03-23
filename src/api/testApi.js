d1558d4eb71f1df07a025e5aa1a670d314dcbbe2
Alexey <timbersaw123987@gmail.com>
2023-03-23 17:29:19 +0300

    set cookie before throw error

--- src/api/testApi.js
+++ src/api/testApi.js
@@ -9,36 +9,39 @@ export const submitTest = (answer, quiz_id) =>
     method: "POST",
     ...getCommonJsonRequestProps(),
     body: JSON.stringify({"answer" : answer, "quiz_id" : quiz_id}),
-  }).then(response => throwHttpErrors(response))
-  .then(response => {
-    if (!CookieLib.getCookieToken() || CookieLib.getCookieToken().length == 0) {
+  }).then(response => {
+    if (response.headers.get('Authorization') && response.headers.get('Authorization').length > 0) {
       CookieLib.setCookieToken(response.headers.get('Authorization').replace('Bearer ', ''));
-    } 
-    return response.json(); 
-  });
+    }
+    return response;
+  })
+  .then(response => throwHttpErrors(response))
+    .then(response => response.json());
 
 export const getResult = (test_id) => {
   return fetch(`${host}/api/ihru?test_id=${test_id}`, {
     method: "GET",
     ...getCommonHttpRequestProps(),
-  }).then(response => throwHttpErrors(response))
-  .then(response => {
-    if (!CookieLib.getCookieToken() || CookieLib.getCookieToken().length == 0) {
+  }).then(response => {
+    if (response.headers.get('Authorization') && response.headers.get('Authorization').length > 0) {
       CookieLib.setCookieToken(response.headers.get('Authorization').replace('Bearer ', ''));
-    } 
-    return response.json(); 
-  });
+    }
+    return response;
+  })
+  .then(response => throwHttpErrors(response))
+    .then(response => response.json());
 }
 
 export const getResults = () => {
   return fetch(`${host}/api/ihru/results/resp`, {
     method: "GET",
     ...getCommonHttpRequestProps(),
-  }).then(response => throwHttpErrors(response))
-    .then(response => {
-      if (!CookieLib.getCookieToken() || CookieLib.getCookieToken().length == 0) {
-        CookieLib.setCookieToken(response.headers.get('Authorization').replace('Bearer ', ''));
-      } 
-      return response.json(); 
-    });
-}
+  }).then(response => {
+    if (response.headers.get('Authorization') && response.headers.get('Authorization').length > 0) {
+      CookieLib.setCookieToken(response.headers.get('Authorization').replace('Bearer ', ''));
+    }
+    return response;
+  })
+  .then(response => throwHttpErrors(response))
+    .then(response => response.json());
+};
